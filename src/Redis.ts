dc2c32c07a97495f8733fae01c2b00cca2b73025
javakh2 <khalilian.javad@gmail.com>
2017-04-20 10:19:30 +0430

    update dependencies

--- src/Redis.ts
+++ src/Redis.ts
@@ -1,40 +1,55 @@
 import {RedisClient, createClient} from "redis";
-import {Database, IDatabaseConfig, ISchemaList, IModelCollection} from "vesta-schema/Database";
-import {IQueryResult, IUpsertResult, IDeleteResult} from "vesta-schema/ICRUDResult";
-import {Vql, Condition} from "vesta-schema/Vql";
-import {IModelValues} from "vesta-schema/Model";
-import {Err} from "vesta-util/Err";
-import {DatabaseError} from "vesta-schema/error/DatabaseError";
+import {
+    Condition,
+    Database, DatabaseError, Err, IDatabaseConfig, IDeleteResult, IModelCollection, IModelValues, IQueryOption,
+    IQueryResult,
+    ISchemaList, IUpsertResult,
+    Vql
+} from "vesta-lib";
 
 export class Redis extends Database {
-    private connection:RedisClient;
-    private schemaList:ISchemaList = {};
-    private models:IModelCollection = {};
-    private config:IDatabaseConfig;
+    close(connection: any): Promise<boolean> {
+        return Promise.reject(false);
+    }
 
-    public connect():Promise<Database> {
+    count<T>(model: string, modelValues: T, option?: IQueryOption): Promise<IQueryResult<T>>;
+    count<T>(query: Vql): Promise<IQueryResult<T>>;
+    count<T>(model, modelValues?, option?): Promise<IQueryResult<T>> {
+        return undefined;
+    }
+
+    increase<T>(model: string, id: number | string, field: string, value: number): Promise<IQueryResult<T>> {
+        return undefined;
+    }
+
+    private connection: RedisClient;
+    private schemaList: ISchemaList = {};
+    private models: IModelCollection = {};
+    private config: IDatabaseConfig;
+
+    public connect(): Promise<Database> {
         if (this.connection) return Promise.resolve(this);
-        return new Promise<Database>((resolve, reject)=> {
-            var client = createClient(this.config.port, this.config.host);
-            client.on('ready', ()=> {
+        return new Promise<Database>((resolve, reject) => {
+            let client = createClient(this.config.port, this.config.host);
+            client.on('ready', () => {
                 this.connection = client;
                 resolve(this);
                 console.log('Redis connection established');
             });
-            client.on('error', (error)=> {
+            client.on('error', (error) => {
                 reject(error);
                 console.log('Redis Error', error);
             });
-            client.on('reconnecting', ()=> {
+            client.on('reconnecting', () => {
                 console.log('Redis connection established');
             });
         })
     }
 
-    constructor(config:IDatabaseConfig, models:IModelCollection) {
+    constructor(config: IDatabaseConfig, models: IModelCollection) {
         super();
-        var schemaList:ISchemaList = {};
-        for (var model in models) {
+        let schemaList: ISchemaList = {};
+        for (let model in models) {
             if (models.hasOwnProperty(model)) {
                 schemaList[model] = models[model].schema;
             }
@@ -44,12 +59,12 @@ export class Redis extends Database {
         this.config = config;
     }
 
-    findById<T>(id:string):Promise<IQueryResult<T|string>> {
-        return new Promise<IQueryResult<T|string>>((resolve, reject)=> {
-            this.connection.get(id, (err, reply)=> {
-                var result:IQueryResult<T|string> = <IQueryResult<T|string>>{};
+    findById<T>(id: string): Promise<IQueryResult<T | string>> {
+        return new Promise<IQueryResult<T | string>>((resolve, reject) => {
+            this.connection.get(id, (err, reply) => {
+                let result: IQueryResult<T | string> = <IQueryResult<T | string>>{};
                 result.items = [];
-                if (err) return reject(new DatabaseError(Err.Code.DBInsert, err.message));
+                if (err) return reject(new DatabaseError(Err.Code.DBInsert, err));
                 if (reply) {
                     try {
                         result.items = [<T>JSON.parse(reply)]
@@ -62,40 +77,40 @@ export class Redis extends Database {
         })
     }
 
-    findByModelValues<T>(model:string, modelValues:IModelValues, limit:number):Promise<IQueryResult<T>> {
+    findByModelValues<T>(model: string, modelValues: IModelValues, limit: number): Promise<IQueryResult<T>> {
         return undefined;
     }
 
-    findByQuery<T>(query:Vql):Promise<IQueryResult<T>> {
+    findByQuery<T>(query: Vql): Promise<IQueryResult<T>> {
         return undefined;
     }
 
-    insertOne<T>(model:string, value:T):Promise<IUpsertResult<T>> {
-        return new Promise<IUpsertResult<T>>((resolve, reject)=> {
-            this.connection.set(model, value, (err)=> {
+    insertOne<T>(model: string, value: T): Promise<IUpsertResult<T>> {
+        return new Promise<IUpsertResult<T>>((resolve, reject) => {
+            this.connection.set(model, value, (err) => {
                 if (err) return reject(new DatabaseError(Err.Code.DBInsert, err.message));
                 resolve();
             });
         })
     }
 
-    insertAll<T>(model:string, values:Array<T>):Promise<IUpsertResult<T>> {
+    insertAll<T>(model: string, values: Array<T>): Promise<IUpsertResult<T>> {
         return undefined;
     }
 
-    updateOne<T>(model:string, value:T):Promise<IUpsertResult<T>> {
+    updateOne<T>(model: string, value: T): Promise<IUpsertResult<T>> {
         return undefined;
     }
 
-    updateAll<T>(model:string, newValues:IModelValues, condition:Condition):Promise<IUpsertResult<T>> {
+    updateAll<T>(model: string, newValues: IModelValues, condition: Condition): Promise<IUpsertResult<T>> {
         return undefined;
     }
 
-    deleteOne(model:string, id:number|string):Promise<IDeleteResult> {
+    deleteOne(model: string, id: number | string): Promise<IDeleteResult> {
         return undefined;
     }
 
-    deleteAll(model:string, condition:Condition):Promise<IDeleteResult> {
+    deleteAll(model: string, condition: Condition): Promise<IDeleteResult> {
         return undefined;
     }
 
@@ -103,7 +118,7 @@ export class Redis extends Database {
         return undefined;
     }
 
-    query<T>(query:string):Promise<T> {
+    query<T>(query: string): Promise<T> {
         return undefined;
-    } 
+    }
 }
